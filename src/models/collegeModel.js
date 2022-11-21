5e6fa33547e0e983e54bb749f4aec4dc082d453e
chhotu24 <chhotushaw24@gmail.com>
2022-11-21 13:43:58 +0530

    done

---
+++ src/models/collegeModel.js
@@ -0,0 +1,20 @@
+const mongoose= require('mongoose')
+
+const collegeSchema= new mongoose.Schema({
+name:{
+type:String,
+unique:true,
+require:true
+},
+fullName:{type:String,
+require:true
+},
+logoLink:{type:String,
+require:true},
+isDeleted:{
+    type:Boolean,
+    default:false
+}
+},{timestamps:true})
+
+module.exports= mongoose.model("college",collegeSchema)
