caf1955a14512909f3b4d01a292351abee2b6600
Saquib Akhtar <saquibmohammad38@gmail.com>
2022-11-21 13:55:34 +0530

    controller done

--- src/index.js
+++
@@ -1,4 +0,0 @@
-const express = require('express')
-const router= express.Router()
-
-const collegeController= require('./controllers/collegeController')
---
+++ src/routes/route.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router= express.Router()
+
+const collegeController= require('../controllers/collegeController')
+
+router.post('/functionup/colleges',collegeController.createCollege)
+
+
+
+module.exports=router
