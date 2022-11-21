0cf5ad66b5631466ce04a1b5d8c50b48859c5b22
chhotu24 <chhotushaw24@gmail.com>
2022-11-21 13:29:39 +0530

    done

---
+++ src/models/internModel.js
@@ -0,0 +1,32 @@
+
+const mongoose= require('mongoose')
+const ObjectId= mongoose.Schema.Types.ObjectId
+const internSchema= new mongoose.Schema({
+
+name:{
+    require:true
+},
+
+email:{
+    type:String,
+    unique:true,
+    require:true
+},
+mobile:{
+    type:Number,
+    unique:true,
+    require:true
+},
+
+collegeId:{
+type:ObjectId,
+ref:"college"
+},
+
+isDelete:{
+    type:Boolean,
+    default:false
+}
+},{timestamps:true})
+
+
